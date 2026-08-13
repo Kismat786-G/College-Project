8378526ff5e77514c7cd9b1f818d839da3dc7561
Kismat Giri <gkismat252@gmail.com>
2026-08-13 19:42:27 +0545

    fixed

--- PHP/PHP/mail_config.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'host' => 'smtp.gmail.com',
-    'port' => 587,
-    'username' => 'gkismat451@gmail.com',
-    'password' => 'bjbu ecaw okuv umkb',
-    'from_email' => 'gkismat451@gmail.com',
-    'from_name' => 'Nepal Travel',
-];
